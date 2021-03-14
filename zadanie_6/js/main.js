88da6888c39e871d4ccaa2c2ac5ece99d38f35c8
dimberd <dimberd777@gmail.com>
2021-03-14 14:25:11 +0100

    add

--- zadanie_6/js/main.js
+++ zadanie_6/js/main.js
@@ -1,4 +1,12 @@
 $(function() {
     mixitup('.gallery__content');
-    $('.slider__item').slick();
+    $('.slider__blog').slick({
+        dots: true,
+        arrows: false,
+        slidersToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    });
+
 });
